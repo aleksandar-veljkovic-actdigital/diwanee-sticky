049b94ee79d928748066cebc596452148f9dcf1b
Aleksandar Veljkovic <aleksandar.veljkovic@diwanee.com>
2016-04-20 12:39:15 +0200

    mpu fix for video page

--- diwanee-sticky-yasmina.js
+++ diwanee-sticky-yasmina.js
@@ -2,7 +2,7 @@
 
 ;// STICKY MPU
 (function () {
-  var offsetTop = ($('.article-video-wrapper').length > 0) ? 185 : 35;
+  var offsetTop = 35;
   $('.desktop #ad-above-fold-MPU-holder').dwSticky({offsetTop: offsetTop, $stEnd: $('#ad-SE-holder')});
 })();
 // eof sticky mpu
